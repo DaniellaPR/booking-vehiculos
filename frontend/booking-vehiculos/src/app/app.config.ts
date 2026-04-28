d81ec4d9043b5be08bfee8b81eaac5cb133de72d
Daniela Pozo <159065844+DaniellaPR@users.noreply.github.com>
2026-04-28 08:43:55 -0500

    Update app.config.ts

--- frontend/booking-vehiculos/src/app/app.config.ts
+++ frontend/booking-vehiculos/src/app/app.config.ts
@@ -2,15 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-// 1. Importa el BASE_PATH de tu carpeta api generada
-import { BASE_PATH } from './core/api';
-// 2. Importa tu environment donde tienes apiUrl: 'https://localhost:7291/api/v1'
-import { environment } from '../environments/environment';
+import { BASE_PATH } from './core/api'; // Tu carpeta de API generada
+import { environment } from '../environments/environment'; // Importa el environment
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    { provide: BASE_PATH, useValue: 'https://localhost:7291' }
+    // CAMBIO AQUÍ: Ahora usa la URL del environment, no localhost
+    { provide: BASE_PATH, useValue: environment.apiUrl }
   ]
 };
